d1ac3823b50b036d71e5fd889b0235b6fc6b1905
lorenzorru0 <lorenzoorru999@gmail.com>
2021-09-02 09:24:26 +0200

    Refact js code

--- js/script.js
+++ js/script.js
@@ -1,64 +1,14 @@
 /* Functions */
 function createPlayground(numCells) {
     let playground = document.getElementById(`playground`);
-    switch(numCells) {
-        case 2:
-            playground.classList.add(`width-2`);
-            for (let i = 0; i < (numCells *numCells); i++) {
-                playground.innerHTML += `<div class="square">${i+1}</div>`;
-            }
-            break;
-        case 3:
-            playground.classList.add(`width-3`)
-            for (let i = 0; i < (numCells * numCells); i++) {
-                playground.innerHTML += `<div class="square">${i+1}</div>`;
-            }
-            break;
-        case 4:
-            playground.classList.add(`width-4`)
-            for (let i = 0; i < (numCells * numCells); i++) {
-                playground.innerHTML += `<div class="square">${i+1}</div>`;
-            }
-            break;
-        case 5:
-            playground.classList.add(`width-5`)
-            for (let i = 0; i < (numCells * numCells); i++) {
-                playground.innerHTML += `<div class="square">${i+1}</div>`;
-            }
-            break;
-        case 6:
-            playground.classList.add(`width-6`)
-            for (let i = 0; i < (numCells * numCells); i++) {
-                playground.innerHTML += `<div class="square">${i+1}</div>`;
-            }
-            break;
-        case 7:
-            playground.classList.add(`width-7`)
-            for (let i = 0; i < (numCells * numCells); i++) {
-                playground.innerHTML += `<div class="square">${i+1}</div>`;
-            }
-            break;
-        case 8:
-            playground.classList.add(`width-8`)
-            for (let i = 0; i < (numCells * numCells); i++) {
-                playground.innerHTML += `<div class="square">${i+1}</div>`;
-            }
-            break;
-        case 9:
-            playground.classList.add(`width-9`)
-            for (let i = 0; i < (numCells * numCells); i++) {
-                playground.innerHTML += `<div class="square">${i+1}</div>`;
-            }
-            break;
-        case 10:
-            playground.classList.add(`width-10`)
-            for (let i = 0; i < (numCells * numCells); i++) {
-                playground.innerHTML += `<div class="square">${i+1}</div>`;
-            }
-            break;
+    let width = `width-${numCells}`;
+    playground.classList.add(width)
+    for (let i = 0; i < (numCells * numCells); i++) {
+        playground.innerHTML += `<div class="square">${i + 1}</div>`;
     }
     playground.classList.add(`view`)
 }
+
 
 
 /* Main program */
